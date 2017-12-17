e5362bad9132c6478fc05f34a95c9f8561714edf
Leam Hall <leamhall@gmail.com>
2017-12-17 07:42:27 -0500

    toys/get_dragon takes a name to search for.

--- toys/get_dragon.rb
+++ toys/get_dragon.rb
@@ -1,12 +1,22 @@
 require 'mongo'
 require 'character'
 require 'presenter'
+require 'optparse'
 
 Mongo::Logger.logger.level = Logger::WARN
 client  = Mongo::Client.new(['localhost:27017'], :database => 'dragons')
 db      = client.database
 
-name = "Tala Domici"
+options = {}
+parser = OptionParser.new do |opts|
+  opts.on('-c character', '--character', 'Select a character') do |c|
+    options[:character] = c
+  end
+end
+parser.parse!
+
+name = options[:character]
+
 collection = client[:dragons]
 collection.find("name" => /#{name}/).each do |d|
   drag = Hash[d]
