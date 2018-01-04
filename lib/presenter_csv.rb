880ec011e0edcfa3588c828c9339ae2d15df697f
Leam Hall <leamhall@gmail.com>
2018-01-03 19:57:37 -0500

    Updated presenter_csv
    It will have the issue if a field isn't present. Look at presenter_default as an example.

--- lib/presenter_csv.rb
+++ lib/presenter_csv.rb
@@ -1,25 +1,47 @@
 module PresenterCSV
 
-
   def PresenterCSV.show(character)
     first_career = true
     first_skill = true
-    print "#{character.name}:"
-    print "#{character.upp}:"
-    print "#{character.age}:"
-    character.careers.each_pair do |career, terms|
-      print "," if first_career == false
-      first_career = false
-      print "#{career}-#{terms}"
+    line = []
+    if character.noble?
+      line << character.title
+    else
+      line << ""
     end
-    print":"
+    line << character.name.split()[0]
+    line << character.name.split()[1]
+    line << character.gender
+    line << character.age
+    line << character.upp
+    line << character.appearence.capitalize
+    line << character.temperament.capitalize
+    plot_line = "#{character.plot[0].capitalize} (#{character.plot[1]})"
+    line << plot_line
+    traits_line = character.traits.map(&:capitalize).join(', ')
+    line << traits_line
+    careers = ""
+    character.careers.each_pair do |career, terms|
+      careers << "," if first_career == false
+      first_career = false
+      careers << "#{career}-#{terms}"
+    end
+    line << careers
+    skills = ""
     if character.skills 
       character.skills.each_pair do |skill, level|
-        print "," if first_skill == false
+        skills << "," if first_skill == false
         first_skill = false
-        print "#{skill}-#{level}"
+        skills << "#{skill}-#{level}"
       end
+      line << skills
+      line << character.stuff['cash']
+      benefit_line = ""
+      character.stuff["benefits"].each do |k,v|
+        benefit_line << "#{k} (#{v}) "
+      end  
+      line << benefit_line
     end
-    print":"
+    puts line.join(":")
   end
 end
