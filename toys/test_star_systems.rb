7c322a17f9dfc00e92bdde6049654ebb14a6263a
Leam Hall <leamhall@gmail.com>
2018-01-05 14:27:48 -0500

    Made test_star_systems more open.

--- toys/test_star_systems.rb
+++ toys/test_star_systems.rb
@@ -2,7 +2,7 @@ $LOAD_PATH << File.expand_path("../../lib", __FILE__)
 
 require "star_systems"
 
+planet = StarSystem.new
 313.times do
-  planet = StarSystem.new
   puts planet.name.capitalize
 end
