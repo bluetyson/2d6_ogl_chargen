71f450ed763843dd0b2667f153d7c2d3f68602a7
Leam Hall <leamhall@gmail.com>
2018-01-27 13:14:03 -0500

    Other career uses array_from_file.

--- lib/careers/other.rb
+++ lib/careers/other.rb
@@ -3,45 +3,11 @@ require "career"
 
 class Other < Career
   def initialize
-    @skill_options = [ 
-      "+1 Str",
-      "+1 Dex",
-      "+1 Edu", 
-      "-1 Soc", 
-      "Brawling", 
-      "Blade", 
-      "Vehicle",
-      "Gambling", 
-      "Brawling", 
-      "Bribery", 
-      "Blade", 
-      "GunCbt", 
-      "Streetwise", 
-      "Mechanical", 
-      "Electronic", 
-      "Gambling", 
-      "Brawling", 
-      "Forgery" 
-      ]
-    @advanced_skill_options = [
-      "Medic",
-      "Computer",
-      "Forgery",
-      "Electronic", 
-      "Liaison",
-      "Streetwise", 
-      "JoT"
-      ]
-
+    @skill_options = array_from_file('other_base_skills.txt')
+    @advanced_skill_options = array_from_file('other_advanced_skills.txt')
     @muster_out_benefits = Hash.new
     @muster_out_benefits["cash"] = [1000, 100000]
-    @muster_out_benefits["benefits"] = [
-      "LowPsg",
-      "+1 Int",
-      "+1 Edu",
-      "Gun",
-      "HighPsg"
-    ]
+    @muster_out_benefits["benefits"] = array_from_file('other_muster_stuff.txt')
   end
  
 end
