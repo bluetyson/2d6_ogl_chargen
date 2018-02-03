d378949ccf645645c5a557863562d1fb23ed3125
Leam Hall <leamhall@gmail.com>
2018-02-02 21:30:15 -0500

    Fixed Squad Leader reference.

--- toys/gang_teams.rb
+++ toys/gang_teams.rb
@@ -8,41 +8,43 @@ client  = Mongo::Client.new(['localhost:27017'], :database => 'people')
 db      = client.database
 
 counter = 0
-leaders = Array.new
-alpha   = Array.new
-bravo   = Array.new
-charlie = Array.new
-delta   = Array.new
+teams   = Hash.new
+teams[:leaders] = Array.new
+teams[:alpha]   = Array.new
+teams[:bravo]   = Array.new
+teams[:charlie] = Array.new
+teams[:delta]   = Array.new
 
 collection  = db[:people]
 collection.find().each { |d|
   p = Hash[d]
   person = Character.new(p)
-  puts counter
   if person.name == "Damien Mora" or person.name == "Carter Shelton"
-    leaders   << person
+    teams[:leaders]   << person
   else
     case counter
     when 0
-      alpha   << person
+      teams[:alpha]   << person
     when 1
-      bravo   << person
+      teams[:bravo]   << person
     when 2 
-      charlie << person
+      teams[:charlie] << person
     when 3
-      delta   << person
+      teams[:delta]   << person
       counter = -1
     end
   end
   counter += 1
 }
 
-[alpha, bravo, charlie, delta].each { |team|
-  puts(team.to_s)
+teams.each_pair { |name, team|
+  if name.to_s == 'leaders'
+    print("=== Squad Leaders  ===\n\n")
+  else 
+    print("=== Fire Team  #{name.capitalize}  ===\n\n")
+  end
+
   team.each { |character|
     Presenter.show(character)
   }
 }
- 
-  
-
