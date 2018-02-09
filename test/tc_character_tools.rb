d653627a38455b65ec2cbdbb37b0580f1467ae90
Leam Hall <leamhall@gmail.com>
2018-02-09 18:13:40 -0500

    Fixed missing comma in tc_character_tools.rb.

--- test/tc_character_tools.rb
+++ test/tc_character_tools.rb
@@ -180,7 +180,7 @@ class TestCharacterTools < Test::Unit::TestCase
 
   def test_upp_mod_very_low
     upp   = {:str => 7, :dex => 7, :end => 7, 
-      :int => 2 :edu => 7, :soc => 7}
+      :int => 2, :edu => 7, :soc => 7}
     assert(upp_mod(upp, :int) == -2)
   end 
 
