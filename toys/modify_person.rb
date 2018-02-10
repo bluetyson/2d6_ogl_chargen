42b12e970c565d442246a08cdb81f9c2fa54d3a7
Leam Hall <leamhall@gmail.com>
2018-02-10 09:18:52 -0500

    modify_person works.

--- toys/modify_person.rb
+++ toys/modify_person.rb
@@ -19,23 +19,12 @@ def update_target(data)
   collection.update_one({"_id" => person['_id']}, 
     {"$set" => 
       {
-      #"upp_s"  => "#{dragon['upp']}", 
-      #"upp"     => new_upp,
-      #"careers" => careers,
-      #"dob"     => dob
       modify  => new_data
       }
     })
 end
 
 collection.find({'background': /Oregund gang/}).each do |person|
-  #terms = (dragon['age'] - 18) / 4
-  #terms = 1 if terms < 2
-  #careers = { dragon['career'] => terms }
-  #yob     = 1416 - dragon['age']
-  # yob * 1000 makes the dates as place work out right. 1397123.
-  #dob     = (yob * 1000) + rand(1..360)
-  #new_upp = upp_s_to_h(dragon['upp'])
   name    = person['name']
   age     = person['age'].to_i
   morale  = ((age - 12) / 2) + 6
